8c249585741c1651931f3e90301bb2ed3158aa32
manuelchs <manuelchavarin@aori.mx>
2019-01-22 22:36:57 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const { src, dest, series, watch } = require('gulp');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass');
+const clean = require('gulp-clean');
+
+const php = require('gulp-connect-php');
+const browserSync = require('browser-sync');
+
+function replaceRoutes() {
+    return src(['src/**/*', '!./src/scss/**/*'])
+  .pipe(replace('#/','/'))
+  .pipe(dest('./dist/'))
+  .pipe(browserSync.stream());
+}
+
+function compileSass() {
+    return src('./src/scss/main.scss')
+    .pipe(sass())
+    .pipe(dest('./dist/css'))
+    .pipe(browserSync.stream());
+}
+
+function cleanFolders() {
+    return src('./dist/scss', {read: false})
+    .pipe(clean())
+}
+
+function phpServer() {
+    php.server({base:'./dist'}, function() {
+        browserSync({
+            proxy: '127.0.0.1:8000',
+            baseDir: './dist',
+            notify: false
+        });
+    });
+}
+
+function reload(cb) {
+    browserSync.reload();
+    cb();
+  }
+
+watch('src/**/*', series(replaceRoutes,compileSass, cleanFolders, reload));
+
+exports.default = series(replaceRoutes,compileSass, cleanFolders, phpServer);
