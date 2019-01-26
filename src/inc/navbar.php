72595fc66fb34b864ca42b1b4c2534ae655f49dc
manuelchs <manuelchavarin@aori.mx>
2019-01-26 12:39:26 -0600

    Navbar inicio

---
+++ src/inc/navbar.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="main-wrapper">
+        <div class="navbar-icon">
+            <span></span>
+        </div>
+        <ul class="no-show">
+            <li><a href="/">inicio</a></li>
+            <li><a href="nosotros">nosotros</a></li>
+            <li><a href="menu">menu</a></li>
+            <li><a href="franquicias">franquicias</a></li>
+            <li><a href="contacto">contacto</a></li>
+        </ul>
+        <div class="logo">
+            <img src="#/images/logo-blanco.png" alt="">
+        </div>
+    </div>
+</nav>
--- src/index.php
+++ src/index.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include("#/inc/head.php") ?>
-    <title>Pastay Pesto - Inicio</title>
+    <?php include("#/inc/head.php") ?>
+    <title>Pasta & Pasto - Inicio</title>
 </head>
 <body>
-Soy un dios
+    <section class="hero hero-home">
+        <?php include("#/inc/navbar.php"); ?>
+        <div class="overlay"></div>
+        <div class="main-text">
+            <h1>tu pasta<br>donde quieras</h1>
+            <h2 class="cursiva">fresca como lechuga</h2>
+            <button class="main-button white">¡yo quiero!</button>
+        </div>
+    </section>
+    <section class="scrolling">
+        <p>inicio</p>
+        <p>inicio</p>
+    </section>
 </body>
 </html>
