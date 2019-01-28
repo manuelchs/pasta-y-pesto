25c49b3a6497c87cc7a831f584fe4ac3c3e16b53
manuelchs <manuelchavarin@aori.mx>
2019-01-27 18:56:47 -0600

    avance de 2da seccion

--- src/index.php
+++ src/index.php
@@ -14,9 +14,30 @@
             <button class="main-button white">¡yo quiero!</button>
         </div>
     </section>
-    <section class="scrolling">
-        <p>inicio</p>
-        <p>inicio</p>
+    <section class="about-landing">
+        <div class="main-wrapper">
+            <div class="info-about">
+                <div class="title-wrapper">
+                    <h2>acerca de<br>nosotros</h2>
+                    <img src="#/images/onda-dark.png" alt="">
+                    <p class="cursiva">descubre</p>
+                </div>
+                <p>
+                Somos un exitoso restaurante de comida rápida gourmet que sirve <b>pasta fresca artesanal</b> en prácticas cajitas de cartón personalizadas. Además de contar con un concepto de cocina abierta que ofrece a todos una experiencia única.
+                </p>
+                <button class="main-button dark">saber más</button>
+            </div>
+            <div class="img-four">
+                <div class="row">
+                    <img src="#/images/crop1.png" alt="">
+                    <img src="#/images/crop2.png" alt="">
+                </div>
+                <div class="row">
+                    <img src="#/images/crop3.png" alt="">
+                    <img src="#/images/crop4.png" alt="">
+                </div>
+            </div>
+        </div>
     </section>
 </body>
 </html>
