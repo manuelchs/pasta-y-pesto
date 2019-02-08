4313dcb0f74fc4f804bef2eecda3f9b8246bcc86
manuelchs <manuelchavarin@aori.mx>
2019-02-07 23:18:44 -0600

    Inicio de la sección nosotros ❌ Falta imagenes y frutas flotantes

---
+++ src/nosotros.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("#/inc/head.php") ?>
+    <title>Pasta & Pasto - Nosotros</title>
+</head>
+<body>
+    <section class="hero hero-nosotros">
+        <?php include("#/inc/navbar.php"); ?>
+        <div class="overlay"></div>
+        <div class="main-text">
+            <h2 class="cursiva">fresca como lechuga</h2>
+            <h1 class="letter-sp">nosotros</h1>
+        </div>
+    </section>
+    <section class="main-section quienes-somos">
+        <div class="main-wrapper">
+            <div class="title-wrapper">
+                <img src="#/images/onda-dark.png" alt="">
+                <div class="quienes-titulo">
+                    <h3 class="cursiva">quienes</h2>
+                    <h2 class="letter-sp">somos</h2>
+                </div>
+                <img src="#/images/onda-dark.png" alt="">
+            </div>
+            <p>Somos un exitoso restaurante de comida rápida gourmet que sirve pasta fresca artesanal en prácticas cajitas de cartón personalizadas. Además de contar con un concepto de cocina abierta que ofrece a todos una experiencia única</p>
+            <div class="mision-vision">
+                <div class="texto">
+                    <h3>visión</h3>
+                    <p>Ser reconocidos como la opción que ofrece la mejor pasta fresca heca al momento en nuestros empaques de cartón y que éstos estén presentes en México y el mundo. </p>
+                    <h3>Misión</h3>
+                    <p>Ofrecer a nuestros clientes una gran experiencia a través de sus sentidos; con pasta artesanal, prácticos empaques, cocina abierta y deliciosos aromas. </p>
+                </div>
+                <div class="images">
+                    <div class="img"></div>
+                    <div class="img"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="main-section filosofia">
+        <div class="main-wrapper">
+            <h3>filosofía</h3>
+            <p>Buscamos siempre ofrecerle a nuestros clientes productos elaborados con ingredientes e insumos de primera calidad, preparando todo al momento y siempre a la vista de ellos. Teniendo siempre una amósfera agradable, visual y autditiva en todas nuestras sucursales con personal cordial atendiéndole.</p>
+        </div>
+    </section>
+    <section class="main-section secreto">
+        <div class="main-wrapper">
+            <div class="title-wrapper">
+                <h3 class="letter-sp">nuestro</h3>
+                <h2 class="cursiva">secreto</h2>
+            </div>
+        </div>
+    </section>
+    <section class="artesanal">
+        <h2>pasta artesanal</h2>
+        <div class="img-artesanal"></div>
+    </section>
+    <?php include("#/inc/footer.php") ?>
+</body>
+</html>
