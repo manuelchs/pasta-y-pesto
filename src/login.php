bb7c425a97cb8b5539a5e998c8d622b4c1734643
manuelchs <manuelchavarin@aori.mx>
2019-04-07 16:46:07 -0500

    login form

--- src/login.php
+++ src/login.php
@@ -6,7 +6,14 @@
 </head>
 <body>
     <div class="login-wrapper">
-        
+        <div class="form-wrapper">
+            <h2 class="main-title letter-sp">iniciar sesión</h2>
+            <form class="form" action="">
+                <input type="text" placeholder="Usuario">
+                <input type="password" placeholder="Contraseña">
+                <button class="main-button dark">ingresar</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
